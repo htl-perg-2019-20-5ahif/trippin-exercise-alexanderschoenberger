4d1f2966d8f23000fe157ac190929755158cb1a9
Alexander Schoenberger <20150068@HTBLAPerg.onmicrosoft.at>
2019-10-15 15:09:07 +0200

    add homework

---
+++ AutomativeTrippinUserCreator/AutomativeTrippinUserCreator/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomativeTrippinUserCreator
+{
+    class User
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string CityName { get; set; }
+        public string Country { get; set; }
+    }
+}
